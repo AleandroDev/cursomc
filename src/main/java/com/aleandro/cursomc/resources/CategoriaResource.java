f858f81d9027ce851e3b69e5c6ead332044e677d
Aleandro Silva <jsaleandro@gmail.com>
2017-12-31 17:41:44 -0200

    REST Testado

---
+++ src/main/java/com/aleandro/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package com.aleandro.cursomc.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando";
+	}
+
+}
