e240db3a686aaf08f898366b104e576d12e005a6
Aleandro Silva <jsaleandro@gmail.com>
2018-01-01 17:31:45 -0200

    EndPoint / cliente/{id} disponivel

---
+++ src/main/java/com/aleandro/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.aleandro.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aleandro.cursomc.domain.Cliente;
+import com.aleandro.cursomc.repositories.ClienteRepository;
+import com.aleandro.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! " + 
+						id + ", tipo: " + Cliente.class.getName());
+		} 
+		return obj;
+	}
+	
+}
