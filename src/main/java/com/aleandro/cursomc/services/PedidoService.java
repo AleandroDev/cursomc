25432988ab5e5ef82c55f7c9b00cb78eb492935f
Aleandro Silva <jsaleandro@gmail.com>
2018-01-09 00:34:35 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/aleandro/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.aleandro.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aleandro.cursomc.domain.Pedido;
+import com.aleandro.cursomc.repositories.PedidoRepository;
+import com.aleandro.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! " + 
+						id + ", tipo: " + Pedido.class.getName());
+		} 
+		return obj;
+	}
+	
+}
