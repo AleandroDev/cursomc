b6adf9bbc92f2309bd14f09f36c3aca5013707a7
Aleandro Silva <jsaleandro@gmail.com>
2018-01-09 00:46:28 -0200

    Atualização JsonMangerReferece(Deletar) Substitur JsonIgnore

--- src/main/java/com/aleandro/cursomc/domain/Produto.java
+++ src/main/java/com/aleandro/cursomc/domain/Produto.java
@@ -1,10 +1,10 @@
 package com.aleandro.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -28,7 +27,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "Produto_Categoria", 
 				joinColumns = @JoinColumn(name = "produto_id"), 
