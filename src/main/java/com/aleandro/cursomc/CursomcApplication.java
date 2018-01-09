f6712ebeb05ab1833f3042623c825820f8627584
Aleandro Silva <jsaleandro@gmail.com>
2018-01-08 22:50:32 -0200

    Pedido , EstadoPagamento e Pagamento

--- src/main/java/com/aleandro/cursomc/CursomcApplication.java
+++ src/main/java/com/aleandro/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.aleandro.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.aleandro.cursomc.domain.Cidade;
 import com.aleandro.cursomc.domain.Cliente;
 import com.aleandro.cursomc.domain.Endereco;
 import com.aleandro.cursomc.domain.Estado;
+import com.aleandro.cursomc.domain.Pagamento;
+import com.aleandro.cursomc.domain.PagamentoComBoleto;
+import com.aleandro.cursomc.domain.PagamentoComCartao;
+import com.aleandro.cursomc.domain.Pedido;
 import com.aleandro.cursomc.domain.Produto;
+import com.aleandro.cursomc.domain.enums.EstadoPagamento;
 import com.aleandro.cursomc.domain.enums.TipoCliente;
 import com.aleandro.cursomc.repositories.CategoriaRepository;
 import com.aleandro.cursomc.repositories.CidadeRepository;
 import com.aleandro.cursomc.repositories.ClienteRepository;
 import com.aleandro.cursomc.repositories.EnderecoRepository;
 import com.aleandro.cursomc.repositories.EstadoRepository;
+import com.aleandro.cursomc.repositories.PagamentoRepository;
+import com.aleandro.cursomc.repositories.PedidoRepository;
 import com.aleandro.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -39,16 +47,19 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-		
-		
-		
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
+		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Grafica");
 		Categoria cat3 = new Categoria(null,"Padaria");
@@ -91,6 +102,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
+			
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy hh:mm");
 		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1,ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
+
+		}
 	}
-}
